2e99abde192483c487579a4ff8b4acb8fba0e89c
Aaron Motacek <aaronmotacek@gmail.com>
2023-04-20 23:01:35 -0500

    handling credits (not enough vs enough for transaction)

--- src/server/api/routers/generate.ts
+++ src/server/api/routers/generate.ts
@@ -1,3 +1,4 @@
+import { TRPCError } from "@trpc/server";
 import { z } from "zod";
 
 import {
@@ -9,14 +10,40 @@ import {
 
 
 export const generateRouter = createTRPCRouter({
-  generateIcon: publicProcedure
+  generateIcon: protectedProcedure
     .input(
       z.object({
         prompt: z.string(),
       })
     )
-    .mutation(({ctx, input}) => {
+    .mutation(async ({ctx, input}) => {
       console.log("we are here", input.prompt)
+
+      const { count } = await ctx.prisma.user.updateMany({
+        where: {
+          id: ctx.session.user.id,
+          credits: { // verify user has enough credits
+            gte: 1,
+          }
+        },
+        data: {
+          credits: {
+            decrement: 1,
+          },
+        },
+      })
+
+      // not enough credits
+      if (count <= 0) {
+        throw new TRPCError({
+          code: "BAD_REQUEST",
+          message: "Not enough credits",
+        })
+      }
+
+      // enough credits
+      // TODO: generate icon
+
       return {
         message: "success",
       }
