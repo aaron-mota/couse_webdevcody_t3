6e658f1fc5b7d1fefdce5f977cee8dc28ad9728d
Aaron Motacek <aaronmotacek@gmail.com>
2023-04-21 19:08:41 -0500

    finished HeroBanner component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -44,16 +44,16 @@ function HeroBanner() {
 
       <Grid item xs={6}>
         <Stack justifyContent="center" alignItems="center">
-          <Box component={"img"} src={"/banner-transparent.png"} sx={{width: 400, height: 300}} alt="an image of a bunch of nice looking icons" />
+          <Box component={"img"} src={"/banner-transparent.png"} sx={{width: "auto", height: 300}} alt="an image of a bunch of nice looking icons" />
         </Stack>
       </Grid>
 
       <Grid item xs={6}>
         <Stack justifyContent="center" alignItems="center" sx={{width: 1, height: 1}}>
-          <Typography variant="h3" component="h1" gutterBottom sx={{fontWeight: 500}}>
+          <Typography variant="h3" component="h1" gutterBottom sx={{fontWeight: 500, opacity: 0.8}}>
             Generate icons with a click of a button
           </Typography>
-          <Typography variant="body2" component="h2" gutterBottom sx={{fontSize: "1.4rem"}}>
+          <Typography variant="body2" component="h2" gutterBottom sx={{fontSize: "1.4rem", opacity: 0.6}}>
             Use AI to generate icons in seconds instead of paying a designer and waiting for them to create them for you.
           </Typography>
         </Stack>
